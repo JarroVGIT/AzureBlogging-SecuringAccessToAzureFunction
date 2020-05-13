83f5abffe6b0b4cdc9a267c984500e5ad9e43017
Jarro van GInkel <jarrovanginkel@outlook.com>
2020-05-13 22:53:09 +0200

    Added ProtectedFunctions

---
+++ ProtectedFunctions/FrontendFunction/PublicAPI/__init__.py
@@ -0,0 +1,35 @@
+import logging
+import azure.functions as func
+import requests
+import os
+
+def get_bearer_token(resource_uri):
+    identity_endpoint = os.environ["IDENTITY_ENDPOINT"]
+    identity_header = os.environ["IDENTITY_HEADER"]
+    token_auth_uri = f"{identity_endpoint}?resource={resource_uri}&api-version=2019-08-01"
+    head_msi = {'X-IDENTITY-HEADER':identity_header}
+
+    resp = requests.get(token_auth_uri, headers=head_msi)
+    access_token = resp.json()['access_token']
+
+    return access_token
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+        access_token = get_bearer_token('https://management.azure.com')
+        headers = {'Authorization': 'Bearer '+access_token}
+        
+        #Replace the URL with the URL from your Backend Function.
+        r = requests.get('https://azureblogging-backend.azurewebsites.net/api/SecretAPI', headers=headers)
+        response_text = ""
+        if r.status_code==200:
+                response_text = r.text
+        else:
+                response_text = "There was a failure, response code: " + r.status_code.__str__()
+                
+        return func.HttpResponse(
+            "This message is generated from our PublicAPI in the Frontend Function. \n"+ response_text,
+            status_code=200    
+        )
+
+
--- UnprotectedFunctions/FrontendFunction/PublicAPI/__init__.py
+++ UnprotectedFunctions/FrontendFunction/PublicAPI/__init__.py
@@ -4,11 +4,10 @@ import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     
+    #Replace the URL with the URL from your Backend Function.
     response = requests.get('https://azureblogging-backend.azurewebsites.net/api/SecretAPI').text
 
     return func.HttpResponse(
             "This message is generated from our PublicAPI in the Frontend Function. \n"+ response,
             status_code=200
     )
-
-
