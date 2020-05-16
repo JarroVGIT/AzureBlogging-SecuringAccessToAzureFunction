4c87e3d49c42d81f507c1ef7d165d7c1f9ad83a7
Jarro van GInkel <jarrovanginkel@outlook.com>
2020-05-16 16:27:06 +0200

    Final commit, article published

--- ProtectedFunctions/FrontendFunction/PublicAPI/__init__.py
+++ ProtectedFunctions/FrontendFunction/PublicAPI/__init__.py
@@ -16,17 +16,20 @@ def get_bearer_token(resource_uri):
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-        access_token = get_bearer_token('https://management.azure.com')
+        
+        #Replace the URL with the URL from your Backend Function App
+        access_token = get_bearer_token('https://azureblogging-backend.azurewebsites.net')
         headers = {'Authorization': 'Bearer '+access_token}
         
-        #Replace the URL with the URL from your Backend Function.
+        #Replace the URL with the URL from your Backend Function SecretAPI.
         r = requests.get('https://azureblogging-backend.azurewebsites.net/api/SecretAPI', headers=headers)
         response_text = ""
+
         if r.status_code==200:
                 response_text = r.text
         else:
-                response_text = "There was a failure, response code: " + r.status_code.__str__()
-                
+                response_text = "There was a failure, response code: " + r.status_code.__str__() +  "\n\n" + r.text
+        
         return func.HttpResponse(
             "This message is generated from our PublicAPI in the Frontend Function. \n"+ response_text,
             status_code=200    
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # AzureBlogging-Securing Access To an Azure Function
-This repository holds the sources used in my article on securing access to Azure Functions using Azure AD.
+This repository holds the Azure Functions used in my article on securing access to Azure Functions using Azure AD.
 
-Note: this is work in progress.
+Please see the original article here on AzureBlogging:
+[https://www.azureblogging.com/securing-access-to-azure-functions-with-azure-ad-authentication-and-authorization/](https://www.azureblogging.com/securing-access-to-azure-functions-with-azure-ad-authentication-and-authorization/)
